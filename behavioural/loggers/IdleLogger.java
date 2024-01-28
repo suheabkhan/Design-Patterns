4d84058578a2439eed115b0aaae894c84893aa0d
Suheab Khan <66318470+suheabkhan@users.noreply.github.com>
2024-01-28 13:34:12 +0530

    chain of responsibility design pattern improvement in idle logger

--- behavioural/loggers/IdleLogger.java
+++ behavioural/loggers/IdleLogger.java
@@ -4,6 +4,6 @@ public class IdleLogger implements CustomLogger{
     // This is the end logger, it will not publish any loggers
     @Override
     public void log(String message, LogLevel logLevel) {
-
+       return;
     }
 }
